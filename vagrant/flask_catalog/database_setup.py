ae4e2b8c932848c0898902e663f2e2aec02c9bea
Tyler McGraw Poff <tyler.poff@outlook.com>
2019-02-14 06:10:59 -0600

    added json support to the models used for our items.

--- vagrant/flask_catalog/database_setup.py
+++ vagrant/flask_catalog/database_setup.py
@@ -14,6 +14,12 @@ class Restaurant(Base):
     __tablename__="restaurant"
     name = Column(String(80), nullable=False)
     id = Column(Integer, primary_key=True)
+    @property
+    def serialize(self):
+        return {
+            "name":self.name,
+            "id":self.id
+        }
 
 class MenuItem(Base):
     __tablename__="menu_item"
@@ -24,7 +30,15 @@ class MenuItem(Base):
     price = Column(String(8))
     restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
     restaurant = relationship(Restaurant)
-
+    @property
+    def serialize(self):
+        return {
+            'name':self.name,
+            'description':self.description,
+            "id":self.id,
+            "price":self.price,
+            'course':self.course
+        }
 
 
 
